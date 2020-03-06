186eaa01b4c0840efe19925961c369b9e5e95014
Villy Volnka <36362446+VillyVonka537@users.noreply.github.com>
2020-03-06 23:12:38 +0300

    Revert "Revert "Update Kata7kyu.py""
    This reverts commit ecbaaf09eb3cd9f31129c80af7f06773449e07f5.

--- Kata7kyu.py
+++ Kata7kyu.py
@@ -1,3 +1,15 @@
+#In this Kata, you will be given a lower case string and your task will be to remove k characters from 
+#that string using the following rule:
+#For example: 
+#solve('abracadabra', 1) = 'bracadabra' # remove the leftmost 'a'.
+#solve('abracadabra', 2) = 'brcadabra'  # remove 2 'a' from the left.
+#solve('abracadabra', 6) = 'rcdbr'      # remove 5 'a', remove 1 'b' 
+
+def solve(st,k): 
+    for letter in sorted(st)[:k]:
+        st = st.replace(letter,'',1)
+    return st
+
 ######################################################################################################################
 
 
