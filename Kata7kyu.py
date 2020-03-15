93194647a16c44a88d2fd0890fe2874ffb34331e
Villy Volnka <36362446+VillyVonka537@users.noreply.github.com>
2020-03-15 09:57:12 +0300

    Update Kata7kyu.py

--- Kata7kyu.py
+++ Kata7kyu.py
@@ -1,3 +1,24 @@
+######################################################################################################################
+
+# Kata 6 kyu: Reverse every other word in the string
+# Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, 
+# while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are apart
+# of the word in this kata.
+
+def reverse_alternate(string):
+  if string == "":
+      return ""
+  else:
+      newArr = []
+      for i in range(len(string.split())):
+          if i%2 != 0:
+              newArr.append(string.split()[i][::-1])
+          else:
+              newArr.append(string.split()[i])
+      return " ".join(newArr)
+
+######################################################################################################################
+
 # Hex Word Sum
 # Description
 # As hex values can include letters A through to F, certain English words can be spelled out, such as CAFE, BEEF, or FACADE. 
