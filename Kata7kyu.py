727789faf242e6cdee7fb39b62c4eaef04a394e8
Villy Volnka <36362446+VillyVonka537@users.noreply.github.com>
2020-03-18 16:20:19 +0300

    Update Kata7kyu.py

--- Kata7kyu.py
+++ Kata7kyu.py
@@ -1,5 +1,19 @@
 ######################################################################################################################
 
+# In this kata, you must create a digital root function.
+# A digital root is the recursive sum of all the digits in a number. Given n, take the sum of the digits of n. 
+#If that value has more than one digit, continue reducing in this way until a single-digit number is produced. 
+#This is only applicable to the natural numbers.
+# Here's how it works:
+# digital_root(16)
+# => 1 + 6
+# => 7
+
+def digital_root(n):
+    return n if n < 10 else digital_root(sum(map(int,str(n))))
+
+######################################################################################################################
+
 # Kata 6 kyu: Reverse every other word in the string
 # Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, 
 # while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are apart
