08960acf35637d805aef45007be724c4d1932c55
Villy Volnka <36362446+VillyVonka537@users.noreply.github.com>
2020-03-18 16:24:24 +0300

    Update Kata7kyu.py

--- Kata7kyu.py
+++ Kata7kyu.py
@@ -1,5 +1,25 @@
 ######################################################################################################################
 
+#Kata 6 kyu. Counting Duplicates
+# Count the number of Duplicates
+# Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric 
+#digits that occur more than once in the input string. The input string can be assumed to contain only alphabets
+#(both uppercase and lowercase) and numeric digits.
+# Example
+# "abcde" -> 0 # no characters repeats more than once
+# "aabbcde" -> 2 # 'a' and 'b'
+
+def duplicate_count(text):
+    text = text.lower()
+    duplicates = []
+    for item in text:
+        if text.count(item) > 1 and item not in duplicates:
+            duplicates.append(item)    
+    return len(duplicates)
+
+######################################################################################################################
+
+#Kata 6 kyu. Sum of Digits / Digital Root
 # In this kata, you must create a digital root function.
 # A digital root is the recursive sum of all the digits in a number. Given n, take the sum of the digits of n. 
 #If that value has more than one digit, continue reducing in this way until a single-digit number is produced. 
