c2826c68a88a9250161f3ce222b01d96a3811711
Villy Volnka <36362446+VillyVonka537@users.noreply.github.com>
2020-03-24 23:37:26 +0300

    Update Kata7kyu.py

--- Kata7kyu.py
+++ Kata7kyu.py
@@ -1,3 +1,20 @@
+######################################################################################################################
+
+# Kata 6 kyu Your order, please
+# Your task is to sort a given string. Each word in the string will contain a single number. This number is 
+# the position the word should have in the result.
+# Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+# If the input string is empty, return an empty string. The words in the input String will only contain 
+# valid consecutive numbers.
+# Examples
+# "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+# "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+
+def order(sentence):
+    return ' '.join(sorted([i for i in sentence.split()], key=lambda i: [int(n) for n in i if n.isdigit()][0]))
+
+######################################################################################################################
+
 # Kata 6 kyu Yes No Yes No
 # Write a code that receives an array of numbers or strings, goes one by one through it while taking one value out, 
 # leaving one value in, taking, leaving, and back again to the beginning until all values are out.
