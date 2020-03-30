9553ce6c1499e1ae55ea3f07b591641f9d703a77
Villy Volnka <36362446+VillyVonka537@users.noreply.github.com>
2020-03-31 00:45:55 +0300

    Update Kata 5 kyu; Simple Pig Latin.py

--- Kata 5 kyu; Simple Pig Latin.py
+++ Kata 5 kyu; Simple Pig Latin.py
@@ -1,8 +1,8 @@
-Kata 5 kyu Simple Pig Latin.py
-Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
-Examples
-pig_it('Pig latin is cool') # igPay atinlay siay oolcay
-pig_it('Hello world !')     # elloHay orldway !
+# Kata 5 kyu Simple Pig Latin.py
+# Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+# Examples
+# pig_it('Pig latin is cool') # igPay atinlay siay oolcay
+# pig_it('Hello world !')     # elloHay orldway !
 
 def pig_it(text):
     new_text = []
