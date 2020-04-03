e0b97025d631651cc7a2f8e51aa034afe1b47218
Villy Volnka <36362446+VillyVonka537@users.noreply.github.com>
2020-04-03 05:10:51 +0300

    Update Kata7kyu.py

--- Kata7kyu.py
+++ Kata7kyu.py
@@ -1,5 +1,18 @@
 ######################################################################################################################
 
+# Kata 6 kyu Array.diff
+# Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+# It should remove all values from list a, which are present in list b.
+# array_diff([1,2],[1]) == [2]
+# If a value is present in b, all of its occurrences must be removed from the other:
+# array_diff([1,2,2,2,3],[2]) == [1,3]
+
+def array_diff(a, b):
+    return [item for item in a if item not in b]
+
+
+######################################################################################################################
+
 # Kata 6 kyu Your order, please
 # Your task is to sort a given string. Each word in the string will contain a single number. This number is 
 # the position the word should have in the result.
