5bfc7bf0a3a78c35ecd4c99adfe9581abb2921c0
Villy Volnka <36362446+VillyVonka537@users.noreply.github.com>
2020-04-23 15:04:35 +0300

    Update Kata7kyu.py

--- Kata7kyu.py
+++ Kata7kyu.py
@@ -1,8 +1,30 @@
+
+# Kata 7 kyu Get the Middle Character
+
+# You are going to be given a word. Your job is to return the middle character of the word. If the word's length
+# is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+# #Examples:
+# Kata.getMiddle("test") should return "es"
+# Kata.getMiddle("testing") should return "t"
+# Kata.getMiddle("middle") should return "dd"
+# Kata.getMiddle("A") should return "A"
+# #Input
+# A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases 
+# due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do
+# not need to worry about your solution timing out.
+# #Output
+# The middle character(s) of the word represented as a string.
+
+def get_middle(s):
+    x = int(len(s)/2)
+    return s[x] if len(s)%2!=0 else s[x-1:x+1]
+
+
 ######################################################################################################################
 
-Sum of odd numbers
+# Kata 7kyu Sum of odd numbers
 
-Given the triangle of consecutive odd numbers:
+#Given the triangle of consecutive odd numbers:
 #              1
 #           3     5
 #        7     9    11
