498596974f9517823ae6b8ee0b691e740a617e89
Villy Volnka <36362446+VillyVonka537@users.noreply.github.com>
2020-04-24 19:13:39 +0300

    Update Kata7kyu.py

--- Kata7kyu.py
+++ Kata7kyu.py
@@ -1,5 +1,20 @@
 ######################################################################################################################
 
+#7 kyu Square Every Digit
+
+# Welcome. In this kata, you are asked to square every digit of a number.
+# For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+# Note: The function accepts an integer and returns an integer
+
+def square_digits(num):
+    num = str(num)
+    result = ''
+    for item in num:
+        result += str(int(item)**2)
+    return int(result)
+
+######################################################################################################################
+
 #Kata 7 kyu Descending Order
 #Your task is to make a function that can take any non-negative integer as a argument and return it with its digits 
 #in descending order. Essentially, rearrange the digits to create the highest possible number.
