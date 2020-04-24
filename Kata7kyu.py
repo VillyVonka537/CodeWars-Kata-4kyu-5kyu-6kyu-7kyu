551f912ccfd0c8add907b67b1ad472bdf611502b
Villy Volnka <36362446+VillyVonka537@users.noreply.github.com>
2020-04-24 19:01:18 +0300

    Update Kata7kyu.py

--- Kata7kyu.py
+++ Kata7kyu.py
@@ -1,3 +1,22 @@
+######################################################################################################################
+
+#Kata 7 kyu Descending Order
+#Your task is to make a function that can take any non-negative integer as a argument and return it with its digits 
+#in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+#Examples:
+#Input: 21445 Output: 54421
+#Input: 145263 Output: 654321
+#Input: 123456789 Output: 987654321
+
+def descending_order(num):
+    num = sorted(list(str(num)), reverse= True)
+    for item in num:
+        item = int(item)
+    return int(''.join(map(str, num)))
+
+######################################################################################################################
+
 # Kata 7 kyu Shortest Word
 # Simple, given a string of words, return the length of the shortest word(s).
 # String will never be empty and you do not need to account for different data types.
