8b58d5a9ff691cf7b14e9fdf258c146f340bdb0b
Villy Volnka <36362446+VillyVonka537@users.noreply.github.com>
2020-04-25 22:34:09 +0300

    Update Kata7kyu.py

--- Kata7kyu.py
+++ Kata7kyu.py
@@ -1,3 +1,25 @@
+#Kata 7 kyu Jaden Casing Strings
+
+# Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). 
+# Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known
+# for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how
+# contractions are expected to be in the example below.
+# Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from 
+# Jaden Smith, but they are not capitalized in the same way he originally typed them.
+# Example:
+# Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+# Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+def to_jaden_case(string):
+    string = string.split(' ')
+    new_string = []
+    for item in string:
+        item = item.capitalize()
+        new_string.append(item)
+    return' '.join(new_string)
+
+######################################################################################################################
+
 #Kata 7 kyu Isograms
 
 # An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that
